86f8e84de80bcfc7b62c233d1cc385a02b139afc
Pavel Ulan <Pavel.Ulan@aristocrat.com>
2022-12-07 09:23:08 +0300

    Task 2 (sprint4) v1.0

--- src/main/java/framework/Const.java
+++
@@ -1,6 +0,0 @@
-package framework;
-
-public class Const {
-    public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
-    public static final String YA_PAGE_URL = "https://dzen.ru/?yredirect=true";
-}
